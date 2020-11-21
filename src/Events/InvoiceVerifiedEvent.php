f93ff4aec52c55b75a2f6320eb28206f1d4c1e5e
Amirhossein Banavi <ah.banavi@gmail.com>
2020-11-21 13:15:36 +0330

    Update InvoiceVerifiedEvent.php

--- src/Events/InvoiceVerifiedEvent.php
+++ src/Events/InvoiceVerifiedEvent.php
@@ -6,7 +6,6 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Shetabit\Multipay\Contracts\DriverInterface;
@@ -15,7 +14,7 @@ use Shetabit\Multipay\Invoice;
 
 class InvoiceVerifiedEvent
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use InteractsWithSockets, SerializesModels;
 
     public $receipt;
     public $driver;
